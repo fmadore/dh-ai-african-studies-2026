613e1c8c27f8de3275883e276c006f6c0b70f9cb
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2025-11-10 14:28:31 +0100

    feat: add participant profiles for Irene Mwendwa, James Yékú, and Nuraddin Aman with images

---
+++ src/lib/data/participants/james-yeku.ts
@@ -0,0 +1,14 @@
+import type { Participant } from '$lib/types/participant';
+
+export const jamesYeku: Participant = {
+	name: 'James Yékú',
+	affiliation: 'University of Kansas',
+	affiliationCoordinates: {
+		latitude: 38.9543,
+		longitude: -95.2558
+	},
+	role: 'Participant',
+	bio: 'James Yékú, a recipient of the Alexander von Humboldt fellowship, teaches postcolonial digital humanities and African literary studies at the University of Kansas as an Associate Professor of African and African American Studies. He is the author of "Cultural Netizenship: Social Media, Popular Culture, and Performance in Nigeria," "The Algorithmic Age of Personality: African Literature and Cancel Culture," as well as two books of poetry, and a nonfiction collection.',
+	researchRegions: ['Nigeria', 'West Africa'],
+	photoUrl: '/images/participants/james-yeku.png'
+};
