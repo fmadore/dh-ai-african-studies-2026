4ae4971f5ab80a8e93234c25480ae6e571cfaa03
Frédérick Madore <93156843+fmadore@users.noreply.github.com>
2026-02-16 07:35:12 +0100

    Add new participant

---
+++ src/lib/data/participants/vera-breitner.ts
@@ -0,0 +1,15 @@
+import type { Participant } from '$lib/types/participant';
+
+export const veraBreitner: Participant = {
+	name: 'Vera Breitner',
+	affiliation: 'Bielefeld University',
+	affiliationCoordinates: {
+		latitude: 52.0385,
+		longitude: 8.4934
+	},
+	country: 'Germany',
+	role: 'Participant',
+	website: 'https://ekvv.uni-bielefeld.de/pers_publ/publ/PersonDetail.jsp?personId=74610648&lang=EN',
+	bio: '',
+	researchRegions: []
+};
